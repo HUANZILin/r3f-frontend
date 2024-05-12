07ebe768f33999b121451f92003ec7d2fec131a8
Karen Lin <karen901208@gmail.com>
2024-05-12 21:53:59 +0800

    Init repo

---
+++ src/components/UI.jsx
@@ -0,0 +1,86 @@
+import { useRef } from "react";
+import { useChat } from "../hooks/useChat";
+
+export const UI = ({ hidden, ...props }) => {
+  const input = useRef();
+  const { chat, loading, cameraZoomed, setCameraZoomed, message } = useChat();
+
+  const sendMessage = () => {
+    const text = input.current.value;
+    if (!loading && !message) {
+      chat(text);
+      input.current.value = "";
+    }
+  };
+  if (hidden) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 bottom-0 z-10 flex justify-between p-4 flex-col pointer-events-none">
+        <div className="self-start backdrop-blur-md bg-white bg-opacity-50 p-4 rounded-lg">
+          <h1 className="font-black text-xl">我的虛擬女友</h1>
+          <p>我的落腮鬍呢？ 😥</p>
+        </div>
+        <div className="flex items-center gap-2 pointer-events-auto max-w-screen-sm w-full mx-auto">
+          <input
+            className="w-full placeholder:text-gray-800 placeholder:italic p-4 rounded-md bg-opacity-50 bg-white backdrop-blur-md"
+            placeholder="請輸入..."
+            ref={input}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+          />
+          <button
+            disabled={loading || message}
+            onClick={sendMessage}
+            className={` bg-lime-600 hover:bg-lime-700 w-40 text-white p-4 px-5 font-semibold rounded-md ${
+              loading || message ? "cursor-not-allowed opacity-30" : ""
+            }`}
+          >
+            傳送
+          </button>
+          <button
+            onClick={() => setCameraZoomed(!cameraZoomed)}
+            className="pointer-events-auto bg-lime-600 hover:bg-lime-700 text-white p-4 rounded-md"
+          >
+            {cameraZoomed ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM13.5 10.5h-6"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6"
+                />
+              </svg>
+            )}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
